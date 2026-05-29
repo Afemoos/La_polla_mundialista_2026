3b7eecd68356c46fe66388d293b73e1cd9c97c69
Afemoos <afemos023@gmail.com>
2026-05-29 15:42:45 -0400

    chore: move firebase config to env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,18 +3,16 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-97druvjgXmPyPri3vSQJokktRqo5KSY",
-  authDomain: "la-polla-mundialista-5f3f5.firebaseapp.com",
-  projectId: "la-polla-mundialista-5f3f5",
-  storageBucket: "la-polla-mundialista-5f3f5.firebasestorage.app",
-  messagingSenderId: "443987046127",
-  appId: "1:443987046127:web:b0e24f508ff793e4c7b51a",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-63HNDGC4NM"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
